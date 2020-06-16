0d89c8e7ec43e9e8a50c63802e8edec3c3881233
Márcio Vinícius <marcio.campos@outlook.com>
2020-06-16 10:55:00 -0300

    criando nsobject

--- eggplant-brownie/Models/Refeicao.swift
+++ eggplant-brownie/Models/Refeicao.swift
@@ -8,17 +8,37 @@
 
 import UIKit
 
-class Refeicao: NSObject {
+class Refeicao: NSObject, NSCoding {
+    
+    // MARK: Atributos
     
     let nome: String
     let felicidade: Int
     var itens: Array<Item> = []
+    
+    // MARK: Init
     
     init(nome: String, felicidade: Int, itens: [Item] = []){
         self.nome = nome
         self.felicidade = felicidade
         self.itens = itens
     }
+    
+    // MARK: - NSCoding
+    
+    func encode(with coder: NSCoder) {
+        coder.encode(nome, forKey: "nome")
+        coder.encode(felicidade, forKey: "felicidade")
+        coder.encode(itens, forKey: "itens")
+    }
+    
+    required init?(coder: NSCoder) {
+        nome = coder.decodeObject(forKey: "nome") as! String
+        felicidade = coder.decodeInteger(forKey: "felicidade")
+        itens = coder.decodeObject(forKey: "itens") as! Array<Item>
+    }
+    
+    // MARK: - Metodos
     
     func totalDeCalorias() -> Double {
         var total = 0.0
